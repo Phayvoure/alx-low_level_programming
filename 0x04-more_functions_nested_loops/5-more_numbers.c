a57202ddaded11b85e5cd8bda6a4fae20ec720a2
Phayvoure <favourakanegbu@gmail.com>
2022-09-15 16:19:37 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 ten times
+ *
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i = 0, a, b;
+
+	while (i < 10)
+	{
+		a = 0;
+		while (a < 15)
+		{
+			b = a;
+			if (a > 9)
+				b = a / 10;
+			_putchar('0' + b);
+			a++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
