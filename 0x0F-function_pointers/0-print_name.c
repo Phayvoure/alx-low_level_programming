ba0323fd6314ac814b2f8549bc15b6075c8d4b51
Phayvoure <favourakanegbu@gmail.com>
2022-10-12 13:58:17 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
-#include "function_pointers.h"
-
 /**
- * print_name - prints name through passed in function pointer
+ * print_name - function to print name
  *
- * @name: character string
- * @f: function pointer
- * Return: nothing
+ * @name: name to print
+ * @f: function pointer that doesn't return anything
+ *
+ * Return: Always 0 (Success)
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
+	if (name && f)
 		f(name);
 }
